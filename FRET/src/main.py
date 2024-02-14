1665c25a7ea9c418aa49e25ef23dfefe2bd4e9b4
Bozhidar Klouchek <klouchek.bozhidar@gmail.com>
2024-02-14 14:10:44 +0000

    Add ability to output to .csv file

--- FRET/src/main.py
+++ FRET/src/main.py
@@ -1,8 +1,8 @@
 from fret_reader import getReqts
 from fret_serialiser import addSerilisationToReqts
 from verifier import verify_serilisations
-import sys, json
 from pathlib import Path
+import sys, json, csv
 
 SAVE_RESULT = True
 
@@ -11,7 +11,15 @@ if(len(reqts) != 0):
     reqts_with_serilisations, serialisedFormulasCount = addSerilisationToReqts(reqts, readFormulasCount)
     verify_serilisations(reqts_with_serilisations, serialisedFormulasCount)
     if(SAVE_RESULT):
+        result = [{k : v for k,v in reqt.items() if k != 'evaluatedSerialisation' and k != 'evaluatedFret'} for reqt in reqts_with_serilisations]
         Path("../output").mkdir(parents=True, exist_ok=True)
+        # Save to .json
         with open('../output/result.json', 'w', encoding='utf-8') as f:
-            json.dump([{k : v for k,v in reqt.items() if k != 'evaluatedSerialisation' and k != 'evaluatedFret'} for reqt in reqts_with_serilisations], f, ensure_ascii=False, indent=4)
+            json.dump(result, f, ensure_ascii=False, indent=4)
+        # Save to .csv
+        with open('../output/result.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(result[0].keys())
+            for res in result:
+                writer.writerow(res.values())
         print('Saved result!')
